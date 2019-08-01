2cac6652f66861c3a6d887a59eb6fc845b535201
Maciekgt540 <42473694+Maciekgt540@users.noreply.github.com>
2019-08-01 08:52:56 +0200

    Python Variables and letters(uppercase, lowercase)

---
+++ Letters/Letter.py
@@ -0,0 +1,15 @@
+#Python is case sensitive!
+
+
+text = "This is a text"
+print(text)
+anotherText = "this is a text"
+
+print(text == anotherText)
+
+'''
+    this is a block comment
+    I can add new lines without #
+'''
+
+print("This is not a comment")
